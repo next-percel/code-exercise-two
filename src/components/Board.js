e74db8978a1fc6da1d5b51b9572596c24c2a10d3
Milind Nayse <milindnayse@gmail.com>
2020-09-01 07:00:53 +0200

    Refactor the win logic

--- src/components/Board.js
+++ src/components/Board.js
@@ -18,29 +18,26 @@ const Board = () => {
   const [gameStatus, setGameStatus] = useState(`${constants.GAME_START}, ${constants.NEXT_PLAYER}: ${constants.PLAYER_X}`)
 
   const getWinner = squares => {
-    if (squares[0] && squares[0] === squares[1] && squares[0] === squares[2]) {
-      return squares[0]
-    }
-    if (squares[3] && squares[3] === squares[4] && squares[3] === squares[5]) {
-      return squares[3]
-    }
-    if (squares[6] && squares[6] === squares[7] && squares[6] === squares[8]) {
-      return squares[6]
-    }
-    if (squares[0] && squares[0] === squares[3] && squares[0] === squares[6]) {
-      return squares[0]
-    }
-    if (squares[1] && squares[1] === squares[4] && squares[1] === squares[7]) {
-      return squares[1]
-    }
-    if (squares[2] && squares[2] === squares[5] && squares[2] === squares[8]) {
-      return squares[2]
-    }
-    if (squares[0] && squares[0] === squares[4] && squares[0] === squares[8]) {
-      return squares[0]
-    }
-    if (squares[2] && squares[2] === squares[4] && squares[2] === squares[6]) {
-      return squares[2]
+    const winPositions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let winPosition = 0; winPosition < winPositions.length; winPosition++) {
+      const [position1, position2, position3] = winPositions[winPosition];
+      
+      const firstPositionFilled = squares[position1]
+      const matchWithSecondPosition = squares[position1] === squares[position2]
+      const matchWithThirdPosition = squares[position1] === squares[position3]
+      
+      if (firstPositionFilled && matchWithSecondPosition && matchWithThirdPosition) {
+        return firstPositionFilled;
+      }
     }
     return null
   }
